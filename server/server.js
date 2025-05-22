bea5c0b1fbbe6da3059ab278a50b16911b9abd23
Your Name <hemanttandan82@gmail.com>
2025-05-22 14:12:56 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.use(xss());
 app.use(mongoSanitize());
 app.use(
   cors({
-    origin: ["https://hemantjob.netlify.app", "http://localhost:3000"],
+    origin: ["hhttps://jobtrackingh.netlify.app", "http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
